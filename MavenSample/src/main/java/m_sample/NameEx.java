50193d077ff866b366f85143cd4a20b565f549b0
jeffk <jeffk@192.168.1.42>
2024-06-06 07:28:45 +0530

    sample commit

---
+++ MavenSample/src/main/java/m_sample/NameEx.java
@@ -0,0 +1,15 @@
+package m_sample;
+
+public class NameEx {
+public void add(int a,int b)
+{
+int c=a+b;	
+System.out.println(c);
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+NameEx obj=new NameEx();
+obj.add(40, 30);
+	}
+
+}
