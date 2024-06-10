cd318cf67970a5bb014191c953c6c5ac504acbfd
jeffk <jeffk@192.168.1.42>
2024-06-10 10:52:01 +0530

    check

---
+++ MavenSample/src/main/java/accessSpecifiers2/Access_Specifier2.java
@@ -0,0 +1,14 @@
+package accessSpecifiers2;
+
+import accessSpecifiers.Access_SpecifiersEx;
+
+public class Access_Specifier2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Access_SpecifiersEx obj=new Access_SpecifiersEx();
+obj.display1();
+//obj.dispaly4(); protected 
+	}
+
+}
---
+++ MavenSample/src/main/java/inheritance/SingleP_Ex.java
@@ -0,0 +1,10 @@
+package inheritance;
+
+public class SingleP_Ex {
+public void number()
+{int a=40;
+int b=30;
+int c=a+b;
+System.out.println(c);
+}
+}
