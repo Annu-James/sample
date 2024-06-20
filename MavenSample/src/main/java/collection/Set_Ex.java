5a175383a7692d43b2f592974c2d3693fdcd87e8
jeffk <jeffk@192.168.1.42>
2024-06-20 18:07:46 +0530

    check 20-6

--- MavenSample/src/main/java/collection/Set_Ex.java
+++ MavenSample/src/main/java/collection/Set_Ex.java
@@ -1,7 +1,7 @@
 package collection;
 import java.util.*;
 public class Set_Ex {
-
+//
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 Set<Integer> obj=new LinkedHashSet();
